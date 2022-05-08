336c39ef399b2bde897927a18f46d4f9802aac6f
anthonycoded <barbels33@gmail.com>
2022-05-07 22:44:04 -0400

    removed unused code

--- src/navigation/AuthNavigator.js
+++ src/navigation/AuthNavigator.js
@@ -3,15 +3,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StyleSheet } from "react-native";
 
 import Header from "../components/Header";
-// import RegisterPath from "../screens/register/RegisterPath";
-// import RegisterFormContainer from "../screens/register/RegisterFormContainer";
-// import NewMemberScreen from "../screens/newMember/NewMemberScreen";
 import LandingScreen from "../screens/landingScreen/LandingScreen";
-import Help from "../screens/landingScreen/Help";
-// import NewMemberFormContainer from "../screens/newMember/NewMemberFormContainer";
-// import ChooseAccountScreen from "../screens/newMember/ChooseAccountScreen";
-// import ForgotPassword from "../screens/landing/ForgotPassword";
-import GoBack from "../components/GoBack";
 
 const AuthNavigator = () => {
   const Stack = createNativeStackNavigator();
@@ -28,83 +20,7 @@ const AuthNavigator = () => {
             headerBackVisible: false,
           }}
         ></Stack.Screen>
-        {/* <Stack.Screen
-          name="Forgot"
-          component={ForgotPassword}
-          options={({ navigation }) => ({
-            headerTitle: (props) => <Header {...props} />,
-            headerTitleAlign: "center",
-            headerStyle: styles.nav,
-            headerBackVisible: false,
-            headerLeft: () => {
-              return <GoBack navigation={navigation} />;
-            },
-          })}
-        ></Stack.Screen>
-        <Stack.Screen
-          name="RegisterPath"
-          component={RegisterPath}
-          options={{
-            headerTitle: (props) => <Header {...props} />,
-            headerTitleAlign: "center",
-            headerStyle: styles.nav,
-            headerBackVisible: false,
-            headerLeft: () => {
-              return null;
-            },
-          }}
-        />
-        <Stack.Screen
-          name="Register"
-          component={RegisterFormContainer}
-          options={{
-            headerTitle: (props) => <Header {...props} />,
-            headerTitleAlign: "center",
-            headerStyle: styles.nav,
-            headerLeft: () => {
-              return null;
-            },
-          }}
-        />
-
-        <Stack.Screen
-          name="NewMemberForms"
-          component={NewMemberFormContainer}
-          options={{
-            headerTitle: (props) => <Header {...props} />,
-            headerTitleAlign: "center",
-            headerStyle: styles.nav,
-            headerLeft: () => {
-              return null;
-            },
-          }}
-        />
-        <Stack.Screen
-          name="NewMemberPath"
-          component={NewMemberScreen}
-          options={{
-            headerTitle: (props) => <Header {...props} />,
-            headerTitleAlign: "center",
-            headerStyle: styles.nav,
-            headerLeft: () => {
-              return null;
-            },
-          }}
-        />
-        <Stack.Screen
-          name="ChooseAccount"
-          component={ChooseAccountScreen}
-          options={{
-            headerTitle: (props) => <Header {...props} />,
-            headerTitleAlign: "center",
-            headerStyle: styles.nav,
-            headerLeft: () => {
-              return null;
-            },
-          }}
-        /> */}
       </Stack.Navigator>
-      <Help></Help>
     </>
   );
 };
