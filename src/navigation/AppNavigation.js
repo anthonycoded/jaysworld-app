cf43bc0db1d9c06d1870e0aadeb0e244cc605743
anthonycoded <barbels33@gmail.com>
2022-05-08 20:09:12 -0400

    unwrraped safe area provider

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { View, Text } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
@@ -9,29 +10,27 @@ const Stack = createNativeStackNavigator();
 
 const AppNavigation = () => {
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="Auth"
-          screenOptions={{
-            headerStyle: {
-              backgroundColor: "white",
-            },
-            headerShown: false,
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Auth"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "white",
+          },
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen
+          name="Drawer"
+          component={DrawerNavigator}
+          options={{
+            headerBackVisible: false,
           }}
-        >
-          <Stack.Screen
-            name="Drawer"
-            component={DrawerNavigator}
-            options={{
-              headerBackVisible: false,
-            }}
-          ></Stack.Screen>
+        ></Stack.Screen>
 
-          <Stack.Screen name="Auth" component={AuthNavigator} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaProvider>
+        <Stack.Screen name="Auth" component={AuthNavigator} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
