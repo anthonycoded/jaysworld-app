22250a5763c3c530369cd7a4f7887cb409ec6186
anthonycoded <barbels33@gmail.com>
2022-06-22 20:43:07 -0400

    readded stripe

--- App.js
+++ App.js
@@ -3,15 +3,17 @@ import { Provider } from "react-redux";
 import store from "./src/store/Store";
 import { StatusBar } from "expo-status-bar";
 import AppWrapper from "./AppWrapper";
-//import { StripeProvider } from "@stripe/stripe-react-native";
+import { StripeProvider } from "@stripe/stripe-react-native";
 
 function App() {
   return (
     <Provider store={store}>
-      {/* <StripeProvider publishableKey="pk_test_51IaNZlCILXbg6WUSGDz5uOCrk6AgKamms3Lpa8DtMAdyakKzcelFTqPzbYDU8H9F21ZJimiH55WOROIUp2hae6sa00G0olzXE0">
-        
-      </StripeProvider>*/}
-      <AppWrapper></AppWrapper>
+      <StripeProvider
+        merchantIdentifier="merchant.com.jaysworld"
+        publishableKey="pk_test_51IaNZlCILXbg6WUSGDz5uOCrk6AgKamms3Lpa8DtMAdyakKzcelFTqPzbYDU8H9F21ZJimiH55WOROIUp2hae6sa00G0olzXE0"
+      >
+        <AppWrapper></AppWrapper>
+      </StripeProvider>
       <StatusBar style="light"></StatusBar>
     </Provider>
   );
