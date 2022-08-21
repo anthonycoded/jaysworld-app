84d78000186a9c37c3afee617c8de0c60f1a2d59
anthonycoded <barbels33@gmail.com>
2022-08-20 23:17:29 -0400

    refactored reducer

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,53 +1,32 @@
-const CHECK_USERNAME_SUCCESS = "CHECK_USERNAME_SUCCESS";
-const CHECK_ANSWER_SUCCESS = "CHECK_ANSWER_SUCCESS";
-const USER_LOGIN_SUCCESS1 = "USER_LOGIN_SUCCESS1";
-const USER_LOGIN_ATTEMPT = "USER_LOGIN_ATTEMPT";
-const USER_LOGIN_FAIL = " USER_LOGIN_FAIL";
-const CLEAR_LOGIN_STATE = "CLEAR_LOGIN_STATE";
-const Check_Auth = "Check_Auth";
+const USER_LOGIN_SUCCESS = "USER_LOGIN_SUCCESS";
+const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+const AUTH_FAILED = " AUTH_FAILED";
+const CLEAR_STATE = "CLEAR_STATE";
 
 //SET INITIAL STATE
 const initialState = {
   loading: false,
   message: null,
   error: null,
-  success: false,
-  status: false,
-  attempts: 0,
-  usernameSuccess: false,
-  questionsSuccess: false,
+  status: undefined,
+  admin: false,
+  user: undefined,
+  id: undefined,
 };
 
 export const authReducer = (state = initialState, action) => {
   switch (action.type) {
-    ///LOGIN ATTEMPT
-    case USER_LOGIN_ATTEMPT:
-      return {
-        ...state,
-        message: null,
-        loading: true,
-        success: false,
-      };
-
-    case Check_Auth:
-      return {
-        ...state,
-        success: action.payload.success,
-        admin: action.payload.admin,
-      };
-
-    case CLEAR_LOGIN_STATE:
+    case CLEAR_STATE:
       return {
         loading: false,
         message: null,
         error: null,
-        success: false,
         status: false,
-        attempts: false,
-        usernameSuccess: false,
-        questionsSuccess: false,
+        admin: false,
+        user: undefined,
+        id: undefined,
       };
-    case USER_LOGIN_SUCCESS1:
+    case USER_LOGIN_SUCCESS:
       let { admin, success, user, token, id } = action.payload;
 
       return {
@@ -59,7 +38,7 @@ export const authReducer = (state = initialState, action) => {
         id,
       };
 
-    case USER_LOGIN_FAIL:
+    case AUTH_FAILED:
       return {
         ...state,
         loading: false,
@@ -67,17 +46,16 @@ export const authReducer = (state = initialState, action) => {
         success: false,
         status: action.payload.status,
       };
-    case CHECK_USERNAME_SUCCESS:
-      return {
-        ...state,
-        usernameSuccess: true,
-      };
 
-    case CHECK_ANSWER_SUCCESS:
+    case REGISTER_SUCCESS: {
       return {
         ...state,
-        questionsSuccess: true,
+        status: "Success",
+        success: true,
+        user: user,
+        admin: false,
       };
+    }
 
     default:
       return state;
