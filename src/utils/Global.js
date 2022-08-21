b676f6a46559d4466518258adbf57ee3cf0a26e0
anthonycoded <barbels33@gmail.com>
2022-08-20 23:17:37 -0400

    updated links

--- src/utils/Global.js
+++ src/utils/Global.js
@@ -1,5 +1,8 @@
-const API_LINK = "http://localhost:8081/";
-
+//const API_LINK = "http://localhost:8081/";
+const API_LINK = "https://jaysworld-cb39f.uc.r.appspot.com/";
 module.exports = {
-  AddToCart: "",
+  Register: API_LINK + "auth/register",
+  Login: API_LINK + "auth/login",
+
+  GetBeats: API_LINK + "beats/get-all",
 };
