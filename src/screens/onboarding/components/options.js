43430289ddf4939681a22e52e4a5a92f832ffb43
anthonycoded <barbels33@gmail.com>
2022-08-29 10:05:21 -0400

    added/updated options

---
+++ src/screens/onboarding/components/options.js
@@ -0,0 +1,53 @@
+export const ethnicityList = [
+  "White",
+  "Black / African American",
+  "Asian",
+  "American Indian",
+  "Native Hawaiian and other Pacific Islander",
+  "Two or more Races",
+  "Hispanic or Latino",
+  "Other",
+  "Prefer not to Disclose",
+];
+
+export const genderList = [
+  "Man",
+  "Woman",
+  "Trans Female",
+  "Trans Male",
+  "Non-Binary",
+  "Other",
+  "Prefer not to Disclose",
+];
+
+export const beatTypes = [
+  "Chill",
+  "Guitar",
+  "Trap",
+  "R&B",
+  "LoFi",
+  "Jazz",
+  "Party",
+  "Turnup",
+  "Vibes",
+  "Mood",
+  "Piano",
+  "Soul",
+];
+
+export const professions = [
+  "Music Lover",
+  "Rapper",
+  "Singer",
+  "Artist",
+  "Producer",
+  "$100,000 +",
+  "Prefer not to Disclose",
+];
+export const maritalList = [
+  "Single",
+  "Married",
+  "Divorced",
+  "Separated",
+  "Widow",
+];
